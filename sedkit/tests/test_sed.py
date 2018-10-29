dbe3c2c7173db8942c28d7fe5cd16843567e930d
Joe Filippazzo <jfilippazzo@stsci.edu>
2018-10-29 10:43:59 -0400

    Added no photometry test

--- sedkit/tests/test_sed.py
+++ sedkit/tests/test_sed.py
@@ -15,13 +15,13 @@ SPEC2 = [np.linspace(21000,38000,150)*q.AA, abs(np.random.normal(size=150))*5E-1
 class TestSED(unittest.TestCase):
     """Tests for the SED class"""
     def setUp(self):
-    
+
         # Make Spectrum class for testing
         self.spec1 = sp.Spectrum(*SPEC1)
         self.spec2 = sp.Spectrum(*SPEC2)
-    
+
         self.sed = sed.SED()
-        
+
     def test_no_spectra(self):
         """Test that a purely photometric SED can be creted"""
         s = copy.copy(self.sed)
@@ -36,11 +36,25 @@ class TestSED(unittest.TestCase):
         s.add_photometry('WISE.W2', 1.143, 0.019)
         s.add_photometry('WISE.W3', -0.067, 0.008)
         s.add_photometry('WISE.W4', -0.127, 0.006)
-        
+
         s.results
-        
+
         self.assertTrue(s.Teff is not None)
-        
+
+    def test_no_photometry(self):
+        """Test that a purely photometric SED can be creted"""
+        s = copy.copy(self.sed)
+        s.age = 455*q.Myr, 13*q.Myr
+        s.radius = 2.362*q.Rsun, 0.02*q.Rjup
+        s.parallax = 130.23*q.mas, 0.36*q.mas
+        s.spectral_type = 'A0V'
+        s.add_spectrum(self.spec1)
+        s.add_spectrum(self.spec1)
+
+        s.results
+
+        self.assertTrue(s.Teff is not None)
+
     # def test_from_database(self):
     #     """Test that an SED can be created from a database"""
     #     s = copy.copy(self.sed)
