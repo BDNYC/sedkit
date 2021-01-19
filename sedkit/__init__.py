b341812804c54f120d672a135a34fb0707325a52
Joe Filippazzo <jfilippazzo@stsci.edu>
2021-01-19 10:18:15 -0500

    Make docs build

--- sedkit/__init__.py
+++ sedkit/__init__.py
@@ -7,8 +7,25 @@ Initialize modules
 
 Author: Joe Filippazzo, jfilippazzo@stsci.edu
 """
+import re
 
 from .catalog import Catalog
 from .sed import SED, VegaSED
 from .spectrum import Spectrum, FileSpectrum, Vega, Blackbody, ModelSpectrum
 from .modelgrid import ModelGrid, BTSettl, SpexPrismLibrary
+
+__version_commit__ = ''
+_regex_git_hash = re.compile(r'.*\+g(\w+)')
+
+__version__ = '1.1.0'
+
+# from pkg_resources import get_distribution, DistributionNotFound
+# try:
+#     __version__ = get_distribution(__name__).version
+# except DistributionNotFound:
+#     __version__ = 'dev'
+
+if '+' in __version__:
+    commit = _regex_git_hash.match(__version__).groups()
+    if commit:
+        __version_commit__ = commit[0]
