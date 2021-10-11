692598dcba67e421176b71f20a6f79cc4d55b71c
Joe Filippazzo <jfilippazzo@stsci.edu>
2021-10-11 14:46:03 -0400

    Fixing tests using Blackbody

--- sedkit/tests/test_sed.py
+++ sedkit/tests/test_sed.py
@@ -4,7 +4,6 @@ from pkg_resources import resource_filename
 
 import numpy as np
 import astropy.units as q
-from astropy.modeling.blackbody import blackbody_lambda
 from astropy.coordinates import SkyCoord
 
 from .. import sed
@@ -16,15 +15,17 @@ class TestSED(unittest.TestCase):
     """Tests for the SED class"""
     def setUp(self):
 
+        # Make Blackbody
+        bb3 = sp.Blackbody(np.linspace(0.8, 2.5, 200) * q.um, 3000*q.K)
+        bb6 = sp.Blackbody(np.linspace(21000, 38000, 150) * q.AA, 6000*q.K)
+
         # Make Spectrum class for testing
-        self.WAVE1 = np.linspace(0.8, 2.5, 200)*q.um
-        self.FLUX1 = blackbody_lambda(self.WAVE1, 3000*q.K)*q.sr
+        self.WAVE1, self.FLUX1 = bb3.spectrum[:1]
         SPEC1 = [self.WAVE1, self.FLUX1, self.FLUX1/100.]
         self.spec1 = sp.Spectrum(*SPEC1)
 
         # Make another
-        WAVE2 = np.linspace(21000, 38000, 150)*q.AA
-        FLUX2 = blackbody_lambda(WAVE2, 6000*q.K)*q.sr
+        self.WAVE2, self.FLUX2 = bb6.spectrum[:1]
         SPEC2 = [WAVE2, FLUX2, FLUX2/100.]
         self.spec2 = sp.Spectrum(*SPEC2)
 
