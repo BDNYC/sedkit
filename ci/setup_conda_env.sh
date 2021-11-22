be690fe0735756c9f3813b6dc24438cd320bb1ea
Joe Filippazzo <jfilippazzo@stsci.edu>
2021-11-22 15:54:21 -0500

    Added healpy conda install

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -3,4 +3,5 @@ echo "Creating conda environment for Python $PYTHON_VERSION"
 conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=sedkit-$PYTHON_VERSION
 source activate $CONDA_ENV
+conda install healpy
 pip install pytest pytest-cov coveralls
