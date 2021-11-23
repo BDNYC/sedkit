48f2ef760ce9747f847e7149d559295d2bbbcac7
Joe Filippazzo <jfilippazzo@stsci.edu>
2021-11-22 21:51:04 -0500

    numpydoc upgrade

--- ci/setup_conda_env.sh
+++ ci/setup_conda_env.sh
@@ -4,6 +4,4 @@ conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
 export CONDA_ENV=sedkit-$PYTHON_VERSION
 source activate $CONDA_ENV
 conda config --add channels conda-forge
-conda install pytest-runner --no-deps
-conda install healpy --no-deps
 pip install pytest pytest-cov coveralls
