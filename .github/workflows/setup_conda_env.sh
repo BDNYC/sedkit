faebcba02b589030a8cfa6236bb24f81e3ae8ac3
Joe Filippazzo <jfilippazzo@stsci.edu>
2022-04-20 23:09:40 -0400

    Trying different CI workflow

--- .github/workflows/setup_conda_env.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-echo "Creating conda environment for Python $PYTHON_VERSION"
-conda env create -f "env/environment-${PYTHON_VERSION}.yml" || exit 1
-export CONDA_ENV=sedkit-$PYTHON_VERSION
-source activate $CONDA_ENV
-conda config --add channels conda-forge
-pip install pytest pytest-cov coveralls
