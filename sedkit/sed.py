Fixed unit conversion and added way to force a specific infer_radius method

sedkit/sed.py
@@ -24,7 +24,7 @@ from astropy.coordinates import Angle, SkyCoord
 from astroquery.vizier import Vizier
 from astroquery.simbad import Simbad
 from astropy.utils.exceptions import AstropyWarning
-from bokeh.io import export_png
+# from bokeh.io import export_png
 from bokeh.plotting import figure, show
 from bokeh.models import HoverTool, Range1d, ColumnDataSource
 from dustmaps.bayestar import BayestarWebQuery
@@ -549,17 +549,21 @@ class SED:
             self.min_spec = (999 * q.um).to(self.wave_units)
             self.max_spec = (0 * q.um).to(self.wave_units)
 
-    def calculate_fbol(self, units=q.erg / q.s / q.cm**2):
+    def calculate_fbol(self, units=None):
         """
         Calculate the bolometric flux of the SED
 
         Parameters
         ----------
         units: astropy.units.quantity.Quantity
-            The target untis for fbol
+            The target units for fbol
         """
+        # Get SED in CGS units
+        app_SED = copy(self.app_SED)
+        app_SED.flux_units = q.erg / q.s / q.cm**2 / q.AA
+
         # Integrate the SED to get fbol
-        fbol = self.app_SED.integrate(units=units)
+        fbol = app_SED.integrate(units=units)
 
         # Set the attribute
         self.fbol = tuple([val for val in fbol] + ['This Work'])
@@ -722,44 +726,34 @@ class SED:
         # Fetch the table (photometry or synthetic_photometry)
         table = getattr(self, '_{}'.format(name))
 
-        # Reset absolute photometry
-        table['abs_flux'] = np.nan
-        table['abs_flux_unc'] = np.nan
-        table['abs_magnitude'] = np.nan
-        table['abs_magnitude_unc'] = np.nan
+        # Set flux cols to correct units
+        for idx, col in zip([4, 5, 8, 9], ['app_flux', 'app_flux_unc', 'abs_flux', 'abs_flux_unc']):
+            if col in table.colnames:
+                table.remove_column(col)
+            table.add_column([np.nan * self.flux_units] * len(table), name=col, index=idx)
 
+        # If any photometry is in the table, calculate the flux values from the magnitudes and put in desired units
         if len(table) > 0:
-
-            # Update the photometry
-            table['eff'] = table['eff'].to(self.wave_units)
-            table['app_flux'] = table['app_flux'].to(self.flux_units)
-            table['app_flux_unc'] = table['app_flux_unc'].to(self.flux_units)
-            table['abs_flux'] = table['abs_flux'].to(self.flux_units)
-            table['abs_flux_unc'] = table['abs_flux_unc'].to(self.flux_units)
-
-            # Get the app_mags
-            m = np.array(table)['app_magnitude']
-            m_unc = np.array(table)['app_magnitude_unc']
 
             # Calculate app_flux values
             for n, row in enumerate(table):
-                app_flux, app_flux_unc = u.mag2flux(row['bandpass'], row['app_magnitude'], sig_m=row['app_magnitude_unc'])
-                table['app_flux'][n] = app_flux.to(self.flux_units)
-                table['app_flux_unc'][n] = app_flux_unc.to(self.flux_units)
+                app_flux, app_flux_unc = u.mag2flux(row['bandpass'], row['app_magnitude'], sig_m=row['app_magnitude_unc'], units=self.flux_units)
+                table['app_flux'][n] = app_flux
+                table['app_flux_unc'][n] = app_flux_unc
 
             # Calculate absolute mags
             if self.distance is not None:
 
                 # Calculate abs_mags
-                M, M_unc = u.flux_calibrate(m, self.distance[0], m_unc, self.distance[1])
-                table['abs_magnitude'] = M
-                table['abs_magnitude_unc'] = M_unc
+                M, M_unc = u.flux_calibrate(row['app_magnitude'], self.distance[0], row['app_magnitude_unc'], self.distance[1])
+                table['abs_magnitude'][n] = M
+                table['abs_magnitude_unc'][n] = M_unc
 
                 # Calculate abs_flux values
                 for n, row in enumerate(table):
-                    abs_flux, abs_flux_unc = u.mag2flux(row['bandpass'], row['abs_magnitude'], sig_m=row['abs_magnitude_unc'])
-                    table['abs_flux'][n] = abs_flux.to(self.flux_units)
-                    table['abs_flux_unc'][n] = abs_flux_unc.to(self.flux_units)
+                    abs_flux, abs_flux_unc = u.mag2flux(row['bandpass'], row['abs_magnitude'], sig_m=row['abs_magnitude_unc'], units=self.flux_units)
+                    table['abs_flux'][n] = abs_flux
+                    table['abs_flux_unc'][n] = abs_flux_unc
 
             if name == 'photometry':
 
@@ -774,6 +768,81 @@ class SED:
 
             # Flag suspicious photometry
             self._flag_photometry()
+
+    # def _calibrate_photometry(self, name='photometry'):
+    #     """
+    #     Calculate the absolute magnitudes and flux values of all rows in the photometry table
+    #
+    #     Parameters
+    #     ----------
+    #     name: str
+    #         The name of the attribute to calibrate, ['photometry', 'synthetic_photometry']
+    #     phot_flag: float
+    #         The survey-survey color to flag as suspicious
+    #     """
+    #     # Reset photometric SED
+    #     if name == 'photometry':
+    #         self.app_phot_SED = None
+    #
+    #     # Fetch the table (photometry or synthetic_photometry)
+    #     table = getattr(self, '_{}'.format(name))
+    #
+    #     # Reset absolute photometry
+    #     table['abs_flux'] = np.nan
+    #     table['abs_flux_unc'] = np.nan
+    #     table['abs_magnitude'] = np.nan
+    #     table['abs_magnitude_unc'] = np.nan
+    #
+    #     if len(table) > 0:
+    #
+    #         # Update the photometry units
+    #         table['eff'], table['app_flux'], table['app_flux_unc'] = u.unit_conversion([table['eff'], table['app_flux'], table['app_flux_unc']], wave_units=self.wave_units, flux_units=self.flux_units)
+    #         table['eff'], table['abs_flux'], table['abs_flux_unc'] = u.unit_conversion([table['eff'], table['abs_flux'], table['abs_flux_unc']], wave_units=self.wave_units, flux_units=self.flux_units)
+    #
+    #         # table['eff'] = table['eff'].to(self.wave_units)
+    #         # table['app_flux'] = table['app_flux'].to(self.flux_units)
+    #         # table['app_flux_unc'] = table['app_flux_unc'].to(self.flux_units)
+    #         # table['abs_flux'] = table['abs_flux'].to(self.flux_units)
+    #         # table['abs_flux_unc'] = table['abs_flux_unc'].to(self.flux_units)
+    #
+    #         # Get the app_mags
+    #         m = np.array(table)['app_magnitude']
+    #         m_unc = np.array(table)['app_magnitude_unc']
+    #
+    #         # Calculate app_flux values
+    #         for n, row in enumerate(table):
+    #             app_flux, app_flux_unc = u.mag2flux(row['bandpass'], row['app_magnitude'], sig_m=row['app_magnitude_unc'])
+    #             app_wave, app_flux, app_flux_unc =
+    #             table['app_flux'][n] = app_flux.to(self.flux_units)
+    #             table['app_flux_unc'][n] = app_flux_unc.to(self.flux_units)
+    #
+    #         # Calculate absolute mags
+    #         if self.distance is not None:
+    #
+    #             # Calculate abs_mags
+    #             M, M_unc = u.flux_calibrate(m, self.distance[0], m_unc, self.distance[1])
+    #             table['abs_magnitude'] = M
+    #             table['abs_magnitude_unc'] = M_unc
+    #
+    #             # Calculate abs_flux values
+    #             for n, row in enumerate(table):
+    #                 abs_flux, abs_flux_unc = u.mag2flux(row['bandpass'], row['abs_magnitude'], sig_m=row['abs_magnitude_unc'])
+    #                 table['abs_flux'][n] = abs_flux.to(self.flux_units)
+    #                 table['abs_flux_unc'][n] = abs_flux_unc.to(self.flux_units)
+    #
+    #         if name == 'photometry':
+    #
+    #             # Make apparent photometric SED with photometry
+    #             app_cols = ['eff', 'app_flux', 'app_flux_unc']
+    #             phot_array = np.array(getattr(self, name)[app_cols])
+    #             phot_array = phot_array[(getattr(self, name)['app_flux'] > 0) & (getattr(self, name)['app_flux_unc'] > 0)]
+    #             self.app_phot_SED = sp.Spectrum(*[phot_array[i] * Q for i, Q in zip(app_cols, self.units)])
+    #
+    #             # Set SED as uncalculated
+    #             self.calculated = False
+    #
+    #         # Flag suspicious photometry
+    #         self._flag_photometry()
 
     def _calibrate_spectra(self):
         """
@@ -1866,7 +1935,7 @@ class SED:
                 self.message('Could not calculate mass without Lbol, M_2MASS.J, or M_2MASS.Ks')
 
 
-    def infer_radius(self, radius_units=q.Rsun, plot=False):
+    def infer_radius(self, radius_units=q.Rsun, infer_from=None, plot=False):
         """
         Estimate the radius from model isochrones given an age and Lbol
 
@@ -1874,6 +1943,8 @@ class SED:
         ----------
         radius_units: astropy.units.quantity.Quantity
             The radius units
+        infer_from: str
+            Infer radius from specific source, ['spt', 'Lbol', 'M_J', 'M_Ks']
         plot: bool
             Plot the relation and the evaluated radius
         """
@@ -1883,7 +1954,7 @@ class SED:
             radius_units = q.Rjup
 
         # Try model isochrones
-        if (self.evo_model is not None) and (self.age is not None) and (self.Lbol_sun is not None):
+        if infer_from == 'evo_model' or (self.evo_model is not None) and (self.age is not None) and (self.Lbol_sun is not None):
 
             # Default
             radius = None
@@ -1901,13 +1972,13 @@ class SED:
         else:
 
             # Try radius(spt) relation
-            if self.radius is None and self.spectral_type is not None:
+            if infer_from == 'spectral_type' or (self.radius is None and self.spectral_type is not None):
 
                 # Infer from Dwarf Sequence
                 self.radius = self.mainsequence.evaluate('radius(spt)', self.spectral_type, yunits=radius_units, fit_local=10, plot=plot)
 
             # Try radius(Lbol) relation
-            elif self.radius is None and self.Lbol_sun is not None:
+            elif (self.radius is None and self.Lbol_sun is not None):
 
                 # Infer from Dwarf Sequence
                 self.radius = self.mainsequence.evaluate('radius(Lbol)', self.Lbol_sun, yunits=radius_units, fit_local=10, plot=plot)
@@ -2865,8 +2936,8 @@ class SED:
                     self.message("{} is an invalid gravity. Please use 'beta' or 'gamma' instead.".format(gravity))
 
             # If radius not explicitly set, estimate it from spectral type
-            if self.spectral_type is not None and self.radius is None:
-                self.infer_radius()
+            # if self.spectral_type is not None and self.radius is None:
+            #     self.infer_radius()
 
             # Update the reference
             self._refs['spectral_type'] = ref

sedkit/spectrum.py
@@ -562,7 +562,7 @@ class Spectrum:
         self._flux_units = flux_units
         self._set_units()
 
-    def integrate(self, units=q.erg / q.s / q.cm**2, n_samples=10):
+    def integrate(self, units=None, n_samples=10):
         """Calculate the area under the spectrum
 
         Parameters
@@ -578,14 +578,30 @@ class Spectrum:
             The integrated flux and uncertainty
         """
         # Make sure the target units are flux units
-        if not u.equivalent(units, q.erg / q.s / q.cm**2):
-            raise TypeError("units must be in flux units, e.g. 'erg/s/cm2'")
+        if units is not None:
+
+            if not (u.equivalent(units, q.erg / q.s / q.cm ** 2) or u.equivalent(units, q.Jy * q.um)):
+                raise TypeError("units must be in flux units, e.g. 'erg/s/cm2' or 'Jy * um'")
+
+        else:
+
+            # F_lambda * lambda
+            if u.equivalent(self.flux_units, q.erg / q.s / q.cm ** 2 / q.AA):
+                units = q.erg / q.s / q.cm ** 2
+
+            elif u.equivalent(self.flux_units, q.Jy):
+                units = q.Jy * q.um
+
+            else:
+                raise TypeError("units must be in flux units, e.g. 'erg/s/cm2' or 'Jy * um'")
 
         # Calculate the factor for the given units
         m = self.flux_units * self.wave_units
 
         # Scrub the spectrum
         spec = u.scrub(self.data)
+
+        # Integrate the spectrum
         val = (np.trapz(spec[1], x=spec[0]) * m).to(units)
 
         if self.unc is None:

sedkit/utilities.py
@@ -445,7 +445,7 @@ def flux_calibrate(mag, dist, sig_m=None, sig_d=None, scale_to=10 * q.pc):
         return [np.nan, np.nan]
 
 
-def flux2mag(flx, bandpass):
+def flux2mag(flx, bandpass, photon=True):
     """Calculate the magnitude for a given flux
 
     Parameters
@@ -454,6 +454,8 @@ def flux2mag(flx, bandpass):
         The flux or (flux, uncertainty)
     bandpass: pysynphot.spectrum.ArraySpectralElement
         The bandpass to use
+    photon: bool
+        Convert energy units to photon counts
     """
     if isinstance(flx, UNITS):
         flx = flx, np.nan * flx.unit
@@ -468,36 +470,18 @@ def flux2mag(flx, bandpass):
     unc = unc or np.nan * unit
 
     # Convert energy units to photon counts
-    flx = (flx * (eff / (ac.h * ac.c)).to(1 / q.erg)).to(unit / q.erg)
-    zp = (zp * (eff / (ac.h * ac.c)).to(1 / q.erg)).to(unit / q.erg)
-    unc = (unc * (eff / (ac.h * ac.c)).to(1 / q.erg)).to(unit / q.erg)
+    if photon:
+
+        eff, flx, unc = unit_conversion([eff, flx, unc], flux_units=zp.unit)
+        flx = (flx * (eff / (ac.h * ac.c)).to(1 / q.erg)).to(zp.unit / q.erg)
+        unc = (unc * (eff / (ac.h * ac.c)).to(1 / q.erg)).to(zp.unit / q.erg)
+        zp = (zp * (eff / (ac.h * ac.c)).to(1 / q.erg)).to(zp.unit / q.erg)
 
     # Calculate magnitude
     m = -2.5 * np.log10((flx / zp).value)
     m_unc = (2.5 / np.log(10)) * (unc / flx).value
 
     return m, m_unc
-
-
-def fnu2flam(f_nu, lam, units=q.erg / q.s / q.cm**2 / q.AA):
-    """
-    Convert a flux density as a function of frequency
-    into a function of wavelength
-
-    Parameters
-    ----------
-    f_nu: astropy.unit.quantity.Quantity
-        The flux density
-    lam: astropy.unit.quantity.Quantity
-        The effective wavelength of the flux
-    units: astropy.unit.quantity.Quantity
-        The desired units
-    """
-    # ergs_per_photon = (ac.h*ac.c/lam).to(q.erg)
-
-    f_lam = (f_nu * ac.c / lam**2).to(units)
-
-    return f_lam
 
 
 def minimize_norm(arr1, arr2, **kwargs):
@@ -773,17 +757,24 @@ def mag2flux(band, mag, sig_m='', units=q.erg / q.s / q.cm**2 / q.AA):
             sig_m = sig_m.value
 
         # Calculate the flux density
-        f = (band.zp * 10**(mag / -2.5)).to(units)
+        f = band.zp * 10**(mag / -2.5)
 
         if isinstance(sig_m, str):
             sig_m = np.nan
 
-        sig_f = (f * sig_m * np.log(10) / 2.5).to(units)
+        # Calculate the uncertainty in the flux density
+        sig_f = f * sig_m * np.log(10) / 2.5
 
-        return np.array([f.value, sig_f.value]) * units
+        # Vals to return
+        vals = [f, sig_f]
 
     except IOError:
-        return np.array([np.nan, np.nan]) * units
+        vals = [np.nan, np.nan]
+
+    # Check flux units
+    vals = unit_conversion([band.wave_eff, vals[0], vals[1]], flux_units=units)
+
+    return vals[1:]
 
 
 def monotonic(x):