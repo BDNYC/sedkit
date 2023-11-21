Implemented changes to provide upper and lower errors to evolutionary model derived parameters. Also, fixed normalization problem when displaying all the individual spectra that compose the sed.

sed.py
@@ -8,13 +8,13 @@ and calculate fundamental and atmospheric parameters
 
 Author: Joe Filippazzo, jfilippazzo@stsci.edu
 """
-
 from copy import copy
 import os
 import shutil
 import time
 import warnings
 
+import itertools
 import astropy.table as at
 import astropy.units as q
 import astropy.io.ascii as ii
@@ -506,7 +506,7 @@ class SED:
         age: sequence
             The age and uncertainty in distance units
         """
-        self._validate_and_set_param('age', age, q.Gyr, True, vmin=0 * q.Myr, vmax=13.8 * q.Gyr)
+        self._validate_and_set_param('age', age, q.Gyr, True, vmin=0 * q.Gyr, vmax=13.8 * q.Gyr)
 
     def _calculate_sed(self):
         """
@@ -705,9 +705,10 @@ class SED:
                 Teff_unc = None
             else:
                 Teff_unc = (Teff * np.sqrt((self.Lbol[1] / self.Lbol[0]).value**2 + (2 * self.radius[1] / self.radius[0]).value**2) / 4.).astype(int)
+                Teff_unc_u = (Teff * np.sqrt((self.Lbol[1] / self.Lbol[0]).value**2 + (2 * self.radius[2] / self.radius[0]).value**2) / 4.).astype(int)
 
             # Update the attribute
-            self.Teff = Teff, Teff_unc, 'This Work'
+            self.Teff = Teff, Teff_unc,Teff_unc_u, 'This Work'
 
     def _calibrate_photometry(self, name='photometry'):
         """
@@ -1878,8 +1879,7 @@ class SED:
                 self.message("Could not calculate surface gravity.")
 
             # Store the value
-            self.logg = [logg[0].round(2), logg[1].round(2), logg[2]] if logg is not None else logg
-
+            self.logg = [logg[0].round(2), logg[1].round(2),logg[2].round(2),logg[3]] if logg is not None else logg
         # No dice
         else:
             self.message('Could not calculate logg without Lbol and age')
@@ -1914,7 +1914,7 @@ class SED:
                 mass = self.evo_model.evaluate(self.Lbol_sun, self.age, 'Lbol', 'mass', plot=plot)
 
             # Store the value
-            self.mass = [mass[0].round(3), mass[1].round(3), mass[2]] if mass is not None else mass
+            self.mass = [mass[0].round(0),  mass[1].round(0), mass[2].round(0), mass[3]] if mass is not None else mass
 
         else:
 
@@ -1980,7 +1980,7 @@ class SED:
                 radius = self.evo_model.evaluate(self.Lbol_sun, self.age, 'Lbol', 'radius', plot=plot)
 
             # Store the value
-            self.radius = [radius[0].round(3), radius[1].round(3), radius[2]] if radius is not None else radius
+            self.radius = [radius[0].round(3), radius[1].round(3),radius[2].round(3),radius[3]] if radius is not None else radius
 
         else:
 
@@ -2039,7 +2039,7 @@ class SED:
                 teff = self.evo_model.evaluate(self.Lbol_sun, self.age, 'Lbol', 'teff', plot=plot)
 
             # Store the value
-            self.Teff = [teff[0].round(0), teff[1].round(0), teff[2]] if teff is not None else teff
+            self.Teff = [teff[0].round(0), teff[1].round(0), teff[2].round(0),teff[3]] if teff is not None else teff
 
         else:
 
@@ -2493,7 +2493,7 @@ class SED:
         self._photometry.sort('eff')
         return self._photometry
 
-    def plot(self, app=True, photometry=True, spectra=True, integral=True, synthetic_photometry=False,
+    def plot(self, app=False, photometry=True, spectra=True, integral=True, synthetic_photometry=False,
              best_fit=True, normalize=None, scale=['log', 'log'], output=False, fig=None,
              color='#3a243b', one_color=False, label=None, **kwargs):
         """
@@ -2582,7 +2582,8 @@ class SED:
 
         # ...or make a new plot
         else:
-            TOOLS = ['pan', 'reset', 'box_zoom', 'wheel_zoom', 'save']
+            # TOOLS = ['pan', 'reset', 'box_zoom', 'wheel_zoom', 'save']
+            TOOLS = ['pan', 'reset', 'box_zoom', 'save']
             xlab = 'Wavelength [{}]'.format(self.wave_units)
             ylab = 'Flux Density [{}]'.format(str(self.flux_units))
             self.fig = figure(width=900, height=400, title=self.name,
@@ -2593,10 +2594,13 @@ class SED:
         # Plot spectra
         if spectra and len(self.spectra) > 0:
 
-            if spectra == 'all':
+            if (spectra == 'all' and app is False):
                 for n, spec in enumerate(self.spectra['spectrum']):
+                    self.fig = spec.plot(fig=self.fig, components=True, const=(spec.flux_calibrate(self.distance).flux/spec.flux))
+            elif (spectra == 'all' and app is True):
+                for n, spec in enumerate(self.spectra['spectrum']):
+                    print(const)
                     self.fig = spec.plot(fig=self.fig, components=True, const=const)
-
             else:
                 self.fig.line(spec_SED.wave, spec_SED.flux * const, color=color, alpha=0.8, legend_label='Spectrum')
 
@@ -2656,9 +2660,9 @@ class SED:
                 mod = mod_fit['full_model']
                 mod.wave_units = self.wave_units
                 if mod_fit['fit_to'] == 'phot':
-                    self.fig.square(mod.wave, mod.flux, alpha=0.3, color=color if one_color else next(col_list), legend_label=mod_fit['label'], size=12)
+                    self.fig.square(mod.wave, mod.flux, alpha=0.9, color=color if one_color else next(col_list), legend_label=mod_fit['label'], size=12)
                 else:
-                    self.fig.line(mod.wave, mod.flux, alpha=0.3, color=color if one_color else next(col_list), legend_label=mod_fit['label'], line_width=2)
+                    self.fig.line(mod.wave, mod.flux, alpha=0.9, color=color if one_color else next(col_list), legend_label=mod_fit['label'], line_width=2)
 
         self.fig.legend.location = "top_right"
         self.fig.legend.click_policy = "hide"
@@ -2736,25 +2740,42 @@ class SED:
         # Get the params
         rows = []
         for param in params:
-
             # Get the values and format
             attr = getattr(self, param, None)
-
             if attr is None:
                 attr = '--'
-
+            # error = '$ ^{+0.004}_{-0.004}$'
             if isinstance(attr, (tuple, list)):
-                val, unc = attr[:2]
-                unit = val.unit if hasattr(val, 'unit') else '--'
-                val = val.value if hasattr(val, 'unit') else val
-                unc = unc.value if hasattr(unc, 'unit') else unc
-                if val < 1E-3 or val > 1e5:
-                    val = float('{:.2e}'.format(val))
-                    if unc is None:
-                        unc = '--'
+                if ((param == 'logg') or (param =='mass') or (param == 'radius') or (param == 'Teff')):
+                    val = attr[0]
+                    lower_err = attr[1]
+                    upper_err = attr[2]
+                    unit = val.unit if hasattr(val, 'unit') else '--'
+                    val = val.value if hasattr(val, 'unit') else val
+                    unc_l = lower_err.value * -1 if hasattr(lower_err, 'unit') else lower_err * -1
+                    unc_u = upper_err.value if hasattr(upper_err, 'unit') else upper_err
+                    if val < 1E-3 or val > 1e5:
+                        if param == 'mass':
+                            val = float('{:.0f}'.format(val))
+                            unc =  str('{:+.0f}'.format(unc_l) + ' ' + '{:+.0f}'.format(unc_u))
+                        else:
+                            val = float('{:.2e}'.format(val))
+                            unc = str('{:-.2e}'.format(unc_l) + ' ' + '{:+.2e}'.format(unc_u))
                     else:
-                        unc = float('{:.2e}'.format(unc))
-                rows.append([param, val, unc, unit])
+                        unc = str('{:-.2f}'.format(unc_l) + ' ' + '{:+.2f}'.format(unc_u))
+                    rows.append([param, val, unc, unit])
+                else:
+                    val, unc = attr[:2]
+                    unit = val.unit if hasattr(val, 'unit') else '--'
+                    val = val.value if hasattr(val, 'unit') else val
+                    unc = unc.value if hasattr(unc, 'unit') else unc
+                    if val < 1E-3 or val > 1e5:
+                        val = float('{:.2e}'.format(val))
+                        if unc is None:
+                            unc = '--'
+                        else:
+                            unc = float('{:.2e}'.format(unc))
+                    rows.append([param, val, unc, unit])
 
             elif isinstance(attr, (str, float, bytes, int)):
                 rows.append([param, attr, '--', '--'])
@@ -2769,6 +2790,62 @@ class SED:
         self.calculated = True
 
         return at.Table(np.asarray(rows), names=('param', 'value', 'unc', 'units'))
+    # def results(self):
+    #     """
+    #     A property for displaying the results
+    #     """
+    #     # Make the SED to get the most recent results
+    #     if not self.calculated:
+    #         self.make_sed()
+    #
+    #     # Get the params to display
+    #     params = copy(self.params)
+    #     print(params)
+    #     # Add best fits
+    #     for name, fit in self.best_fit.items():
+    #         params.append(name)
+    #
+    #     # Get the params
+    #     rows = []
+    #     for param in params:
+    #
+    #         # Get the values and format
+    #         attr = getattr(self, param, None)
+    #
+    #         if attr is None:
+    #             attr = '--'
+    #
+    #         if isinstance(attr, (tuple, list)):
+    #             val,lower_unc,upper_unc = attr[:3]
+    #             unit = val.unit if hasattr(val, 'unit') else '--'
+    #             val = val.value if hasattr(val, 'unit') else val
+    #             lower_unc = lower_unc.value if hasattr(lower_unc, 'unit') else lower_unc
+    #             upper_unc = upper_unc.value if hasattr(upper_unc, 'unit') else upper_unc
+    #             if val < 1E-3 or val > 1e5:
+    #                 val = float('{:.2e}'.format(val))
+    #                 if lower_unc is None:
+    #                     lower_unc = '--'
+    #                 else:
+    #                     lower_unc = float('{:.2e}'.format(lower_unc))
+    #                 if upper_unc is None:
+    #                     upper_unc = '--'
+    #                 else:
+    #                     upper_unc = float('{:.2e}'.format(upper_unc))
+    #             rows.append([param, val, lower_unc,upper_unc, unit])
+    #
+    #         elif isinstance(attr, (str, float, bytes, int)):
+    #             rows.append([param, attr, '--', '--','--'])
+    #
+    #         elif hasattr(attr, 'deg'):
+    #             rows.append([param, attr.deg, '--', '--','deg'])
+    #
+    #         else:
+    #             pass
+    #
+    #     # Set as calculated
+    #     self.calculated = True
+    #
+    #     return at.Table(np.asarray(rows), names=('param', 'value', 'lower_unc','upper_unc', 'units'))
 
     def run_methods(self, method_list):
         """
@@ -2997,6 +3074,7 @@ class SED:
         """
         self._validate_and_set_param('Teff', teff, q.K, True, vmin=0 * q.K, vmax=50000 * q.K)
 
+    # def _validate_and_set_param(self, param, values, units, set_uncalculated=True, trigger=[], vmin=None, vmax=None):
     def _validate_and_set_param(self, param, values, units, set_uncalculated=True, trigger=[], vmin=None, vmax=None):
         """
         Method to validate and set a calculated quantity
@@ -3028,7 +3106,6 @@ class SED:
             self.message("Setting {} to 'None'".format(param))
 
         else:
-
             # If the last value is string, it's the reference
             if isinstance(values[-1], str):
                 ref = values[-1]
@@ -3037,16 +3114,17 @@ class SED:
                 ref = None
 
             # Make sure it's a sequence
-            if not u.issequence(values, length=[2, 3]):
+            if not u.issequence(values, length=[2, 3, 4, 5]):
                 raise TypeError("{} must be a sequence of (value, error) or (value, lower_error, upper_error).".format(param))
 
             # Make sure it's in correct units
             if not all([u.equivalent(val, units) for val in values]):
+                print(values)
                 raise TypeError("{} values must be {}".format(param, 'unitless' if units is None else "astropy.units.quantity.Quantity of the appropriate units , e.g. '{}'".format(units)))
 
             # Ensure valid range but don't throw error
-            vmin = vmin or -np.inf * (units or 1)
-            vmax = vmax or np.inf * (units or 1)
+            vmin = vmin or (-np.inf * (units or 1))
+            vmax = vmax or (np.inf * (units or 1))
             if (values[0] < vmin) or (values[0] > vmax):
                 self.message("{}: {} value is not in valid range [{}, {}].".format(values, param, vmin, vmax))
 

utilities.py
@@ -467,7 +467,8 @@ def flux2mag(flx, bandpass, photon=True):
     unit = flx.unit
 
     # Set uncertainty
-    unc = unc or np.nan * unit
+
+    # unc = (unc or (np.nan * unit))
 
     # Convert energy units to photon counts
     if photon: