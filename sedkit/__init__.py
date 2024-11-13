c9ba023523e5d4bde2a116a35fbe1b1c45448b25
Kelle Cruz <kellecruz@gmail.com>
2024-11-13 15:35:37 +0100

    Change to dynamic versioning (#124)
    * remove versioning code
    
    * add setuptools_scm table

--- sedkit/__init__.py
+++ sedkit/__init__.py
@@ -4,28 +4,8 @@
 # !python3
 """
 Initialize modules
-
-Author: Joe Filippazzo, jfilippazzo@stsci.edu
 """
-import re
-
-from .catalog import Catalog
-from .sed import SED, VegaSED
-from .spectrum import Spectrum, FileSpectrum, Vega, Blackbody
-from .modelgrid import ModelGrid, BTSettl, SpexPrismLibrary
-
-__version_commit__ = ''
-_regex_git_hash = re.compile(r'.*\+g(\w+)')
-
-__version__ = '1.2.5'
-
-# from pkg_resources import get_distribution, DistributionNotFound
-# try:
-#     __version__ = get_distribution(__name__).version
-# except DistributionNotFound:
-#     __version__ = 'dev'
-
-if '+' in __version__:
-    commit = _regex_git_hash.match(__version__).groups()
-    if commit:
-        __version_commit__ = commit[0]
+from sedkit.catalog import Catalog
+from sedkit.sed import SED, VegaSED
+from sedkit.spectrum import Spectrum, FileSpectrum, Vega, Blackbody
+from sedkit.modelgrid import ModelGrid, BTSettl, SpexPrismLibrary
